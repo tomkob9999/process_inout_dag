b9546a8ceda374b29ccb62e30783c26002f1877b
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2023-12-21 20:55:19 +0900

    1.0.11
    - beautify the graph

--- DataJourneyDAG.py
+++ DataJourneyDAG.py
@@ -1,4 +1,4 @@
-# Verwsion: 1.0.10
+# Verwsion: 1.0.11
 # Last Update: 2023/12/21
 # Author: Tomio Kobayashi
 
@@ -394,18 +394,12 @@ class DataJourneyDAG:
         maxheight = max([v for k, v in colpos.items() if v != 0])
         newpos = {}
         for k, v in position.items():
-            newheight = v[1]
-#             if colpos[v[0]] == 1:
-#                 newheight = 0.5
-#             else:
-#                 newheight = (newheight)/(colpos[v[0]]-1)
-                
-            if colpos[v[0]] != maxheight:
-                if maxheight == 2 and colpos[v[0]] == 1:
-                    newheight = 0.5
-                else:
-                    newheight = maxheight/(colpos[v[0]]+1)*(newheight+1)
+            
+            gap = (maxheight) / colpos[v[0]]
+            newheight = (gap/2) + v[1]*gap
+            
             newpos[k] = (v[0], newheight)
+            
         position = newpos
 
         selected_vertices1 = list(selected_vertices1)
@@ -477,20 +471,10 @@ class DataJourneyDAG:
         maxheight = max([v for k, v in colpos.items() if v != 0])
         newpos = {}
         for k, v in position.items():
-            newheight = v[1]
-
-#             if colpos[v[0]] == 1:
-#                 newheight = 0.5
-#             else:
-#                 newheight = (newheight)/(colpos[v[0]]-1)
-                
-            if colpos[v[0]] != maxheight:
-                if maxheight == 2 and colpos[v[0]] == 1:
-                    newheight = 0.5
-                else:
-                    newheight = maxheight/(colpos[v[0]]+1)*(newheight+1)
-                    
+            gap = (maxheight) / colpos[v[0]]
+            newheight = (gap/2) + v[1]*gap
             newpos[k] = (v[0], newheight)
+            
         position = newpos
 
         selected_vertices1 = list(selected_vertices1)
@@ -501,8 +485,6 @@ class DataJourneyDAG:
 
         title = "Data Offsprings (" + str(len(set([v[0] for k, v in position.items() if self.dic_vertex_names[k][0:5] != "proc_"]))-1) + " stages)"
         self.draw_selected_vertices_reverse_proc(self.G_T, selected_vertices1,selected_vertices2, selected_vertices3, title=title, node_labels=node_labels, pos=position, reverse=True)
-
-
 
 
 
