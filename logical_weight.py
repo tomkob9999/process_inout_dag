0069d156becf001b44dc3416c95abdc23a77b7e0
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-02-03 11:14:17 +0900

    1.0.2
    - reverted to the original approach to calculate the probability of each chosen

--- logical_weight.py
+++ logical_weight.py
@@ -1,7 +1,7 @@
 # logical_weight
 #
-# Version: 1.0.1
-# Last Update: 2024/02/02
+# Version: 1.0.2
+# Last Update: 2024/02/03
 # Author: Tomio Kobayashi
 #
 # Average of max and min of multiple random variables.  In case of error, securely. max and min are returned
@@ -15,7 +15,19 @@ class logical_weight:
     def get_avg_max(rands, std_ratio=0.25):
         try:
             sds = [np.abs(r * std_ratio) for r in rands]
-            max_prb = np.array(rands)/sum(rands)
+
+            max_prb = [0.0]*len(rands)
+            max_value = max(rands)
+            max_indices = [index for index, value in enumerate(rands) if value == max_value]
+            i = max_indices[0]
+            for j in range(len(rands)):
+                if j in max_indices:
+                    continue
+                max_prb[j] = stats.norm.cdf(0, loc=(rands[i] - rands[j]), scale=(sds[i]+sds[j])) 
+
+            maxmax = (1 - sum(max_prb))/len(max_indices)
+            for m in max_indices:
+                max_prb[m] = maxmax
             return max(sum([(stats.norm(loc=(rands[i]), scale=(sds[i])).ppf(1-max_prb[i]/2)) * max_prb[i] for i in range(len(rands))]), max(rands))
 
         except Exception as e:
@@ -25,7 +37,20 @@ class logical_weight:
     def get_avg_max_nonzero(rands, std_ratio=0.25, sigma=0.5):
         try:
             sds = [np.abs(r * std_ratio) for r in rands]
-            max_prb = np.array(rands)/sum(rands)
+
+            max_prb = [0.0]*len(rands)
+            max_value = max(rands)
+            max_indices = [index for index, value in enumerate(rands) if value == max_value]
+            i = max_indices[0]
+            for j in range(len(rands)):
+                if j in max_indices:
+                    continue
+                max_prb[j] = stats.norm.cdf(0, loc=(rands[i] - rands[j]), scale=(sds[i]+sds[j])) 
+
+            maxmax = (1 - sum(max_prb))/len(max_indices)
+            for m in max_indices:
+                max_prb[m] = maxmax
+
             return max(sum([(stats.lognorm(sigma, scale=rands[i]).ppf(1-max_prb[i]/2)) * max_prb[i] for i in range(len(rands))]), max(rands))
 
         except Exception as e:
@@ -34,7 +59,20 @@ class logical_weight:
     def get_avg_min(rands, std_ratio=0.25):
         try:
             sds = [np.abs(r * std_ratio) for r in rands]
-            min_prb = np.array(rands)/sum(rands)
+
+            min_prb = [0.0]*len(rands)
+            min_value = min(rands)
+            min_indices = [index for index, value in enumerate(rands) if value == min_value]
+
+            i = min_indices[0]
+            for j in range(len(rands)):
+                if j in min_indices:
+                    continue
+                min_prb[j] = stats.norm.cdf(0, loc=(rands[j] - rands[i]), scale=(sds[i]+sds[j])) 
+            minmin = (1 - sum(min_prb))/len(min_indices)
+            for m in min_indices:
+                min_prb[m] = minmin
+            
             return min(sum([(stats.norm(loc=(rands[i]), scale=(sds[i])).ppf(min_prb[i]/2)) * min_prb[i] for i in range(len(rands))]), min(rands))
 
         except Exception as e:
@@ -44,7 +82,20 @@ class logical_weight:
     def get_avg_min_nonzero(rands, std_ratio=0.25, sigma=0.5):
         try:
             sds = [np.abs(r * std_ratio) for r in rands]
-            min_prb = np.array(rands)/sum(rands)
+
+            min_prb = [0.0]*len(rands)
+            min_value = min(rands)
+            min_indices = [index for index, value in enumerate(rands) if value == min_value]
+
+            i = min_indices[0]
+            for j in range(len(rands)):
+                if j in min_indices:
+                    continue
+                min_prb[j] = stats.norm.cdf(0, loc=(rands[j] - rands[i]), scale=(sds[i]+sds[j])) 
+            minmin = (1 - sum(min_prb))/len(min_indices)
+            for m in min_indices:
+                min_prb[m] = minmin
+                
             return min(sum([(stats.lognorm(sigma, scale=rands[i]).ppf(min_prb[i]/2)) * min_prb[i] for i in range(len(rands))]), min(rands))
         except Exception as e:
             return min(rands)
