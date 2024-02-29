406959bc7e7c6a15dd100b8bc1465dc33fc7ee45
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-02-29 12:19:48 +0900

    1.1.6
    -refactored

--- logical_weight.py
+++ logical_weight.py
@@ -1,7 +1,7 @@
 # logical_weight
 #
-# Version: 1.1.5
-# Last Update: 2024/02/28
+# Version: 1.1.6
+# Last Update: 2024/02/29
 # Author: Tomio Kobayashi
 #
 # Average of max and min of multiple random variables.  In case of error, securely. max and min are returned
@@ -15,47 +15,35 @@ class logical_weight:
         self.dic_exp = {}
         
     def lognormal(mu, sigma):
-        return np.log(np.random.lognormal(mu, min(mu, sigma))+1)
+#         return np.log(np.random.lognormal(mu, min(mu, sigma))+1)
+        if mu > 500:
+            f = np.log(np.random.lognormal(500, sigma)+1)
+            ret = mu + f - 500
+        else:
+            ret = np.log(np.random.lognormal(mu, sigma)+1)
+            
+        if ret == float('inf'):
+            return mu
+        else:
+            return ret
     
-    def get_avg_max(rands, sigma=1.):
-        nloop = 3000
+    def get_avg_max(rands, sigma=1., nloop=3000):
         return np.mean([max([np.random.normal(r, sigma) for r in rands]) for i in range(nloop)])
 
     # Good for non-zero values.  Long tailed
-    def get_avg_max_nonzero(rands, sigma=1.):
-        nloop = 3000
-        if max(rands) > 300000:
-            rands2 = [r/300000 for r in rands]
-            ret = np.mean([max([np.log(np.random.lognormal(r, sigma)+1) for r in rands2]) for i in range(nloop)])
-            ret *= 300000
-        elif max(rands) > 500:
-            rands2 = [r/500 for r in rands]
-            ret = np.mean([max([np.log(np.random.lognormal(r, sigma)+1) for r in rands2]) for i in range(nloop)])
-            ret *= 500
-        else:
-            ret = np.mean([max([np.log(np.random.lognormal(r, sigma)+1) for r in rands]) for i in range(nloop)])
+    def get_avg_max_nonzero(rands, sigma=1., nloop=3000):
+        ret = np.mean([max([logical_weight.lognormal(r, sigma) for r in rands]) for i in range(nloop)])
         if ret == float('inf'):
             return max(rands)
         else:
             return ret
 
-    def get_avg_min(rands, sigma=1):
-        nloop = 3000
+    def get_avg_min(rands, sigma=1, nloop=3000):
         return np.mean([min([np.random.normal(r, sigma) for r in rands]) for i in range(nloop)])
 
     # Good for non-zero values.  Long tailed
-    def get_avg_min_nonzero(rands, sigma=1.):
-        nloop = 3000
-        if min(rands) > 300000:
-            rands2 = [r/300000 for r in rands]
-            ret = np.mean([min([np.log(np.random.lognormal(r, sigma)+1) for r in rands2]) for i in range(nloop)])
-            ret *= 300000
-        elif min(rands) > 500:
-            rands2 = [r/500 for r in rands]
-            ret = np.mean([min([np.log(np.random.lognormal(r, sigma)+1) for r in rands2]) for i in range(nloop)])
-            ret *= 500
-        else:
-            ret = np.mean([min([np.log(np.random.lognormal(r, sigma)+1) for r in rands]) for i in range(nloop)])
+    def get_avg_min_nonzero(rands, sigma=1., nloop=3000):
+        ret = np.mean([min([logical_weight.lognormal(r, sigma) for r in rands]) for i in range(nloop)])
         if ret == float('inf'):
             return min(rands)
         else:
@@ -98,7 +86,7 @@ class logical_weight:
 #         return float(exp) 
         return logical_weight.get_avg_max_nonzero([float(exp)], sigma=sigma) if use_lognormal else logical_weight.get_avg_max([float(exp)], sigma=sigma)
 
-    def calc_avg_result_weight_memo(self, inp_exp, weights, use_lognormal=False, loop_limit=2000, sigma=1.):
+    def calc_avg_result_weight_memo(self, inp_exp, weights, use_lognormal=False, loop_limit=2000, nloop=3000, sigma=1.):
 
         exp = inp_exp
 
@@ -125,19 +113,19 @@ class logical_weight:
             match = re.search(pattern, exp)
             if match:
                 val_list = [float(s) for s in match.group().replace("(", "").replace(")", "").replace(" ", "").replace("&", ",").replace("|", ",").split(",")]
-                out_len = logical_weight.get_avg_max_nonzero(val_list, sigma=sigma) if use_lognormal else logical_weight.get_avg_max(val_list, sigma=sigma)
+                out_len = logical_weight.get_avg_max_nonzero(val_list, sigma=sigma, nloop=nloop) if use_lognormal else logical_weight.get_avg_max(val_list, sigma=sigma, nloop=nloop)
                 exp = re.sub(pattern, str(out_len), exp, count=1)
             else:
                 pattern = pattern_or
                 match = re.search(pattern, exp)
                 if match:
                     val_list = [float(s) for s in match.group().replace("(", "").replace(")", "").replace(" ", "").replace("&", ",").replace("|", ",").split(",")]
-                    out_len = logical_weight.get_avg_min_nonzero(val_list, sigma=sigma) if use_lognormal else logical_weight.get_avg_min(val_list, sigma=sigma)
+                    out_len = logical_weight.get_avg_min_nonzero(val_list, sigma=sigma, nloop=nloop) if use_lognormal else logical_weight.get_avg_min(val_list, sigma=sigma, nloop=nloop)
                     exp = re.sub(pattern, str(out_len), exp, count=1)
                 else:
                     break
 #         return float(exp) 
         
-        res = logical_weight.get_avg_max_nonzero([float(exp)], sigma=sigma) if use_lognormal else logical_weight.get_avg_max([float(exp)], sigma=sigma)
+        res = logical_weight.get_avg_max_nonzero([float(exp)], sigma=sigma, nloop=nloop) if use_lognormal else logical_weight.get_avg_max([float(exp)], sigma=sigma, nloop=nloop)
         self.dic_exp[start_exp] = res
         return res
